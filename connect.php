ece166cff2a4d2e6dd6dc11bd86ec21992a75439
HamzaB0 <120846008+HamzaB0@users.noreply.github.com>
2023-05-06 06:55:43 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "foxilitrix";
+$password = "root";
 $dbname = "app";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
